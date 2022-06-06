0b531e8bcc4d26af44d73885e28750fcaee86138
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-06 18:47:28 +0300

    commit tree c02fc7d09a0ed3587bfa608b036de972becd31f3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+const companyname = 'const companyName';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
